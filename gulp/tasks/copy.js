77c96c74ba560b14daa6bd9d745019dcf939cab8
Martin Geno <geno+dev@fireorbit.de>
2017-10-15 22:53:00 +0200

    [TASK] config for Freifunk Bremen

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,8 +1,12 @@
 module.exports = function (gulp, plugins, config) {
   return function copy() {
-    gulp.src(['html/*.html', 'assets/favicon/*'])
-      .pipe(gulp.dest(config.build));
-    gulp.src(['assets/logo.svg', 'service-worker.js'])
+    gulp.src([
+      'html/*.html',
+      'assets/favicon/*',
+      'assets/logo.svg',
+      'assets/grafana-logo.png',
+      'service-worker.js'
+    ])
       .pipe(gulp.dest(config.build));
     gulp.src(['polyfill.js'])
       .pipe(gulp.dest(config.build + '/vendor'));
